6cc78c4085a49d802228b57280626521964144a7
Nils Kalchhauser <nils@studio19.at>
2016-10-21 16:54:20 +0200

    salt-call via docker exec

--- build-container.sh
+++ build-container.sh
@@ -5,7 +5,8 @@ TAG=latest
 
 docker build -t $NAME:$TAG .
 docker push $NAME:$TAG
-container_id=$(docker create -v $(pwd)/salt/states:/srv/salt $NAME:$TAG salt-call --local state.apply)
-docker start -a $container_id
+container_id=$(docker create -v $1:/srv/salt $NAME:$TAG)
+docker start $container_id
+docker exec $container_id salt-call --local state.apply
 docker commit $container_id $NAME:$TAG
 docker push $NAME:$TAG
